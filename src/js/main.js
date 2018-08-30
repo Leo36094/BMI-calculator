a0c1666c989930283df7e879f9e14e467777664a
Leo Chuang <leo36094@gmail.com>
2018-08-30 19:25:55 +0800

    calculator done

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,31 @@
 // BMI值計算公式: BMI = 體重(公斤) / 身高2(公尺2)
-
-const resultBtn = document.querySelector('.btn');
+let status = '';
+const resultBtn = document.querySelector('#btn');
+const rmBtn = document.querySelector('.rmBtn')
 const list = document.querySelector('.list');
 let data = JSON.parse(localStorage.getItem('datalist')) || [];
 
 
 // EventLister
 
-resultBtn.addEventListener('click', calculate,false);
+resultBtn.addEventListener('click', calculate);
 updateInfo(data);
+rmBtn.addEventListener('click', remove);
 
 
 function calculate(e) {
     e.preventDefault();
-    let height = document.querySelector('.height').value;
-    let weight = document.querySelector('.weight').value;
-    const getBMI = weight / (height * height)*0.1;
+    let height = Number(document.querySelector('.height').value);
+    const heightMeter = height * 0.01;//cm to m
+    let weight = Number(document.querySelector('.weight').value);
+    const getBMI = (weight / (heightMeter * heightMeter)).toFixed(1);
+
+    // Value Check
+    if (isNaN(height) || isNaN(weight) || height == ''|| weight == '') {
+        alert('please enter number')
+        return;
+    };
+
     let info = {
         height: height,
         weight: weight,
@@ -24,15 +34,16 @@ function calculate(e) {
     data.push(info);
     updateInfo(data);
     localStorage.setItem('datalist', JSON.stringify(data));
+    window.location.reload();
 }
 
 function updateInfo(info) {
     let str = '';
     for (let i = 0; i < info.length; i++){
-        str +=
-            `
-            <li>
-                <span></span>
+        str +=`
+            <span class="${changeClass(info[i].BMI)}"></span>
+            <li data-num="${i}">
+                <span>${status}</span>
                 <span>BMI: ${info[i].BMI}</span>
                 <span>Height: ${info[i].height} cm</span>
                 <span>Weight: ${info[i].weight} kg</span>
@@ -44,10 +55,40 @@ function updateInfo(info) {
 
 }
 
+//get Status
+
+function changeClass(BMI) {
+    if (BMI >=40) {
+        status = '過度肥胖';
+        return 'overWeight';
+    } else if (BMI >= 35) {
+        status = '中度肥胖';
+        return 'fat';
+    } else if (BMI >= 30) {
+        status = '輕度肥胖';
+        return 'heavy';
+    } else if (BMI >= 25) {
+        status = '肥胖'
+        return 'littleHeavy'
+    } else if (BMI >= 18.5) {
+        status = '理想'
+        return 'ideal'
+    } else {
+        status = '過輕';
+        return 'skinny'
+    }
+}
+
 function getDate() {
-    let date = new Date("Aug 30, 2018");
-    let str = "";
-   str += (date.getMonth() + 1) + "/";
-   str += date.getDate() + "/";
-   str += date.getFullYear();
+    const today = new Date();
+    const time = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+    return time;
+}
+
+//Remove data records
+function remove() {
+    localStorage.clear();
+    data = [];
+    updateInfo(data);
+
 }
